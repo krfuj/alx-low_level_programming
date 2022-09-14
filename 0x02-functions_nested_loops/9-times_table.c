c4306a1763103e8e921ee5036ba91aa02a42afd0
krfuj <presleyingolo@gmail.com>
2022-09-14 13:44:09 -0700

    multiplicationtable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table -prints tables
+ */
+void times_table(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a <= 9)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			c = a * b;
+			printf("%d,  ", c);
+			b++;
+		}
+		printf("\n");
+		a++;
+	}
+
+}
